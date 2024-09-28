52893813beb2853ce1ac2c2d706778849aa2e93f
Joel Castillo Caballero <castillocaballeroj@iestptrujillo.net>
2024-09-28 01:58:32 -0500

    Mi commit castillocaballlero

--- WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06082281ffe2bd4435544e20ca0afd967b27082a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
