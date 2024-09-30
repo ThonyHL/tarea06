c6eb16d68c79c375ef1cd1b98ee4271adb641c29
Joel Castillo <castillocaballeroj@iestptrujillo.net>
2024-09-30 10:03:34 -0500

    Mi coomit Joel Castillo

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52893813beb2853ce1ac2c2d706778849aa2e93f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d0bea41f289ff6697685635d131ad5fc283911")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
